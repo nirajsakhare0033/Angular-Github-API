2268094146ba61bd4a7cf89b92388b96bab47a60
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-18 10:35:48 +0530

    style component and routing done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'MyApp';
+  thikness=10;
+  color = '';
+
+  randomColor() {
+    let hex = '#' + Math.floor(Math.random() * 1231122).toString(15);
+    this.color=hex;
+  }
 }
