8ce8613c0a44e8eb70928356abfdd50c43e75e8d
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-18 09:55:50 +0530

    created projects and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
